f8526c7e91d80ac302478ac00435feea84d91bc5
Супер <@DESKTOP-OMG2SFV>
2023-08-19 13:11:10 +0500

    Изменение кодировки

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+п»ї#include <iostream>
 #include <cstdlib>
 #include <ctime>
 
@@ -6,8 +6,8 @@
 int main() {
 	setlocale(LC_ALL, "RU");
 
-	// Задача 1.
-	std::cout << "Задача 1. \nОдномерный массив размером в 10 элементов: \n\n";
+	// Р—Р°РґР°С‡Р° 1.
+	std::cout << "Р—Р°РґР°С‡Р° 1. \nРћРґРЅРѕРјРµСЂРЅС‹Р№ РјР°СЃСЃРёРІ СЂР°Р·РјРµСЂРѕРј РІ 10 СЌР»РµРјРµРЅС‚РѕРІ: \n\n";
 	const int size1 = 10;
 	int arr[size1]{};
 	int sum = 0;
@@ -17,17 +17,17 @@ int main() {
 		std::cout << arr[i] << ' ';
 		sum += arr[i];
 	}
-	std::cout << "\n\nСред.ариф. массива = " << sum / double(size1) << '\n';
+	std::cout << "\n\nРЎСЂРµРґ.Р°СЂРёС„. РјР°СЃСЃРёРІР° = " << sum / double(size1) << '\n';
 	int k = 0;
 	for (int i = 0; i < size1; i++)
 		if (arr[i] < sum / double(size1)) {
 			k++;
 		}
-	std::cout << "\nКол-во элементов, значение которых меньше средн.ариф.= " << k << std::endl;
+	std::cout << "\nРљРѕР»-РІРѕ СЌР»РµРјРµРЅС‚РѕРІ, Р·РЅР°С‡РµРЅРёРµ РєРѕС‚РѕСЂС‹С… РјРµРЅСЊС€Рµ СЃСЂРµРґРЅ.Р°СЂРёС„.= " << k << std::endl;
 	
 	
-	//Задача 2.
-	std::cout << "Задача 2. \n\nМассив №1\n";
+	//Р—Р°РґР°С‡Р° 2.
+	std::cout << "Р—Р°РґР°С‡Р° 2. \n\nРњР°СЃСЃРёРІ в„–1\n";
 	const int rows1 = 5;
 	const int cols1 = 5;
 	int arr1[rows1][cols1]{};
@@ -45,8 +45,8 @@ int main() {
 			arr1[i][j] = rand() % 10 + 1;
 			sum_arr1 += arr1[i][j];
 		}
-	std::cout << " \nСумма массива №1 = " << sum_arr1;
-	std::cout << "\n\nМассив №2\n";
+	std::cout << " \nРЎСѓРјРјР° РјР°СЃСЃРёРІР° в„–1 = " << sum_arr1;
+	std::cout << "\n\nРњР°СЃСЃРёРІ в„–2\n";
 	const int rows2 = 5;
 	const int cols2 = 5;
 	int arr2[rows2][cols2]{};
@@ -63,19 +63,19 @@ int main() {
 			arr1[i][j] = rand() % 10 + 1;
 			sum_arr2 += arr1[i][j];
 		}
-	std::cout << "\nСумма массива №2 = " << sum_arr2 << "\n\n";
-	std::cout << "Сумма массивов №1 и №2 = " << sum_arr1 + sum_arr2 << std::endl;
+	std::cout << "\nРЎСѓРјРјР° РјР°СЃСЃРёРІР° в„–2 = " << sum_arr2 << "\n\n";
+	std::cout << "РЎСѓРјРјР° РјР°СЃСЃРёРІРѕРІ в„–1 Рё в„–2 = " << sum_arr1 + sum_arr2 << std::endl;
 
 
-	// Задача 3. 
-	std::cout << "Задача 3. \n Ведите шестизначное число -> ";
+	// Р—Р°РґР°С‡Р° 3. 
+	std::cout << "Р—Р°РґР°С‡Р° 3. \n Р’РµРґРёС‚Рµ С€РµСЃС‚РёР·РЅР°С‡РЅРѕРµ С‡РёСЃР»Рѕ -> ";
 	int i;
 	std::cin >> i;
 	const int size = 6;
 	int arr3[size]{};
 	
 	if (i < 100000 || i > 999999) {
-		std::cout << "Программа завершена!";
+		std::cout << "РџСЂРѕРіСЂР°РјРјР° Р·Р°РІРµСЂС€РµРЅР°!";
 	}
 	else
 	for (int i = 0; i < size; i++) {
